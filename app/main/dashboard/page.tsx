dd514d83cf0ef06270b61322ebb136951edb41db
Jpi <130180675+JohnPatoriku@users.noreply.github.com>
2024-11-30 08:26:16 +0800

    30.11.24.0826

--- app/main/dashboard/page.tsx
+++ app/main/dashboard/page.tsx
@@ -118,6 +118,7 @@ export default function Dashboard() {
       setTasks(updatedTasks);
     } else {
       setErrorMessage("This task is locked and cannot be deleted.");
+      onOpen();
     }
   };
 
