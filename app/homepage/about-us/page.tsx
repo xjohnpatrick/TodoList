32e72efc7730be904613a75d377d4c4bf3c3846a
Jpi <130180675+JohnPatoriku@users.noreply.github.com>
2024-12-20 12:49:13 +0800

    20.12.24.1249

--- app/homepage/about-us/page.tsx
+++ app/homepage/about-us/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import patrickImg from "@/public/patrick.jpeg"
 import ianMarcoImg from "@/public/ianMarcoImg.jpg"
+import { IoMdRocket } from "react-icons/io";
 
 export default function AboutUs() {
     const teamMembers = [
@@ -19,9 +20,21 @@ export default function AboutUs() {
       },
     ];
   return (
-    <div className="flex flex-col items-center justify-center w-full h-[1500px] lg:h-[1000px] bg-white-50 font-poppins gap-4">
-      <div className="flex flex-col w-64 lg:w-[800px] items-center text-center gap-4">
-        <h1 className="text-6xl font-semibold">
+    <div className="flex flex-col items-center lg:justify-center w-full h-[1500px] lg:h-[1000px] bg-white-50 font-poppins gap-4">
+      <div className="flex flex-col w-64 sm:w-[600px] lg:w-[800px] items-center text-center gap-4 relative mt-24 lg:mt-0">
+        <div className="hidden md:flex md:w-[750px] lg:w-[950px] xl:w-[1000px] h-20 absolute">
+          <div className="flex justify-between w-full">
+            <IoMdRocket size={60} className="-rotate-12 text-purple-100" />
+            <IoMdRocket size={60} className="rotate-12 text-purple-100" />
+          </div>
+        </div>
+        <div className="hidden md:flex md:w-[730px] lg:w-[930px] xl:w-[1000px] h-20 absolute bottom-0">
+          <div className="flex justify-between w-full">
+            <IoMdRocket size={60} className="-rotate-45 text-purple-100" />
+            <IoMdRocket size={60} className="rotate-45 text-purple-100" />
+          </div>
+        </div>
+        <h1 className="text-4xl lg:text-6xl font-semibold">
           We&apos;re in it for the flow.
         </h1>
         <p className="text-lg">
@@ -45,7 +58,9 @@ export default function AboutUs() {
             <div className="flex flex-col w-full h-full items-center justify-center">
               <span className="text-4xl font-semibold">{item.name}</span>
               <span className="text-lg">{item.description}</span>
-              <span className="text-xs sm:text-sm text-gray-50/80">{item.textLink}</span>
+              <span className="text-xs sm:text-sm text-gray-50/80">
+                {item.textLink}
+              </span>
             </div>
             <div className="flex justify-center items-center h-full w-full">
               <div className="flex rounded-full w-20 h-20 lg:w-40 lg:h-40 relative border-1">
